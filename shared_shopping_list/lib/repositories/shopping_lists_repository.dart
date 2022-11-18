a9382bdcff3556ad0234791d990679aaa789d036
Dominik Vagala <37017489+dvagala@users.noreply.github.com>
2022-11-18 10:10:01 +0000

    Update shopping_lists_repository.dart

--- shared_shopping_list/lib/repositories/shopping_lists_repository.dart
+++ shared_shopping_list/lib/repositories/shopping_lists_repository.dart
@@ -1,3 +1,7 @@
+// In repository we typically access persistnant data, but here we will use just a variable for storing data 
+// Thanks to the ReactiveServiceMixin we can use this class to hold a 'state' and notifies UI that this state changes and it should redraw the screen.
+// More specifically, it notify the ShoppingListsViewModel who explicitly want to listen to this ShoppingListRepository. It notifies him, he notifies View, and redraw would happen and a updated value of 'state' will be shown to the screen. 
+
 import 'package:shared_shopping_list/app/app.locator.dart';
 import 'package:shared_shopping_list/extensions/extensions.dart';
 import 'package:shared_shopping_list/models/shopping_list.dart';
