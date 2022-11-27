4998e6b5cb9a13539842bb1670b626e624a6e998
BravoMotaFCT <jn.mota@campus.fct.unl.pt>
2022-11-27 15:36:17 +0000

    Create new Account Screen

---
+++ shared_shopping_list/lib/UI/screens/create_new_account/create_new_account_view.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:shared_shopping_list/UI/global/blue_button.dart';
+import 'package:shared_shopping_list/UI/global/rounded_outlined_card.dart';
+import 'package:shared_shopping_list/UI/global/screen_header.dart';
+import 'package:shared_shopping_list/UI/global/blue_button.dart';
+import 'package:shared_shopping_list/UI/screens/create_new_shopping_list/create_new_shopping_list_viewmodel.dart';
+import 'package:shared_shopping_list/UI/screens/shopping_list/shopping_list_viewmodel.dart';
+import 'package:shared_shopping_list/UI/screens/shopping_lists/local_widgets/shopping_list_brief_info.dart';
+import 'package:shared_shopping_list/UI/screens/shopping_lists/shopping_lists_viewmodel.dart';
+import 'package:stacked/stacked.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../../../constants/custom_colors.dart';
+import '../../global/default_button.dart';
+import '../shopping_list/local_widgets/delete_from_list_button.dart';
+import 'create_new_account_viewmodel.dart';
+
+class CreateNewAccountView extends ViewModelBuilderWidget<CreateNewAccountViewModel> {
+  const CreateNewAccountView({Key? key}) : super(key: key);
+
+ // final String listId;
+
+  @override
+  CreateNewAccountViewModel viewModelBuilder(BuildContext context) =>
+      CreateNewAccountViewModel();
+
+  @override
+  Widget builder(BuildContext context, CreateNewAccountViewModel viewModel, Widget? child) {
+    TextEditingController usernameController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    TextEditingController confirmPasswordController = TextEditingController();
+
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        //bottom: false,
+        child: Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image.asset(
+                        "assets/images/logo.png",
+                        width: 200,
+                        height: 200,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ScreenHeader(text: "Create new account"),
+                    ],
+                  ),
+                  const SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        child: TextField(
+                          controller: usernameController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Username",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      //const SizedBox(width: 10),
+                      SizedBox(
+                        width: 300,
+                        child: TextField(
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                        ),
+
+                      ),
+                      //const SizedBox(width: 20)
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      //const SizedBox(width: 10),
+                      SizedBox(
+                        width: 300,
+                        child: TextField(
+                          controller: confirmPasswordController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Confirm password',
+                          ),
+                        ),
+
+                      ),
+                      //const SizedBox(width: 20)
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children:[
+                        DefaultButton(text: "Sign up", color: CustomColors.logoDarkBrown)
+                      ]
+                  ),
+                  const SizedBox(height: 30),
+
+                ]
+            )
+
+        ),
+      ),
+    );
+  }
+}
---
+++ shared_shopping_list/lib/UI/screens/create_new_account/create_new_account_viewmodel.dart
@@ -0,0 +1,30 @@
+import 'package:shared_shopping_list/app/app.locator.dart';
+import 'package:shared_shopping_list/repositories/shopping_lists_repository.dart';
+import 'package:shared_shopping_list/services/random_generator_service.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import '../../../models/shopping_list.dart';
+
+class CreateNewAccountViewModel extends BaseViewModel {
+  final _shoppingListsRepository = locator<ShoppingListsRepository>();
+  final _randomGeneratorService = locator<RandomGeneratorService>();
+  final _navigationService = locator<NavigationService>();
+
+  ShoppingList getListById(String id){
+    return _shoppingListsRepository.getListById(id);
+  }
+
+  void removeParticipant(String listId, int index) {
+    _shoppingListsRepository.removeParticipantFromShoppingList(listId, index);
+  }
+
+  void addNewRandomShoppingListAndGoBack() {
+    final newShoppingList = _randomGeneratorService.generateShoppingList();
+    _shoppingListsRepository.addNewShoppingList(newShoppingList);
+
+    _navigationService.back();
+
+
+  }
+}
